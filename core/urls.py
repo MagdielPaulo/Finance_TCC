249060c4aea1e3e6cf45f4c0402f6ec08eb8fd03
fx-space <mirandafranklin32@gamil.com>
2023-07-12 21:19:05 -0300

    gerenciamento

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('perfil/', include('perfil.url'))
+    path('perfil/', include('perfil.urls'))
 ]
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+    essencial = models.BooleanField(default=False) 
+    valor_planejamento = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.categoria  
+    
+class Conta(models.Model):
+    banco_choices = (
+        ('NU', 'Nubank'),
+        ('CE', 'Caixa econômica'),
+    )

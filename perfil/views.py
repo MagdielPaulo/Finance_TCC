4841eccf76807b21c5dcdfafc6acbc4431a2376c
fx-space <mirandafranklin32@gamil.com>
2023-07-17 23:12:50 -0300

    cadastrar

--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Conta
+from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -9,12 +9,13 @@ def home(request):
 
 def gerenciar(request):
     contas = Conta.objects.all()
+    Categorias = Categoria.objects.all()
     #total_contas = contas.aggregate(Sum('valor'))
     total_contas = 0
 
     for conta in contas:
         total_contas += conta.valor
-    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas})
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': Categorias})
 
 
 def cadastrar_banco(request):
@@ -38,8 +39,26 @@ def cadastrar_banco(request):
 
     conta.save()
 
-    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
-        messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
-        return redirect('/perfil/gerenciar/')
+    messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
 
+def deletar_banco(request, id):
+    conta = Conta.objects.get(id=id)
+    conta.delete()
+    
+    messages.add_message(request, constants.SUCCESS, 'Conta removida com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
     return redirect('/perfil/gerenciar/')

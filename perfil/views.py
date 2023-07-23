4397b4ec0c119c2e68b84ded7dab3dff46f23752
fx-space <mirandafranklin32@gamil.com>
2023-07-23 14:51:13 -0300

    dashboard

--- perfil/views.py
+++ perfil/views.py
@@ -4,6 +4,7 @@ from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
 from .utils import calcula_total
+from extrato.models import Valores
 
 def home(request):
     contas = Conta.objects.all()
@@ -78,6 +79,11 @@ def dashboard(request):
     categorias = Categoria.objects.all()
 
     for categoria in categorias:
-        dados[categoria.categoria] = Valores.objects.filter(categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+        total = 0
+        valores = Valores.objects.filter(categoria=categoria)
+        for v in valores:
+            total = total + v.valor 
+        
+        dados[categoria.categoria] = total
 
     return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
